288c26e74ce07a567c99ddd0bf135132cc16eb91
BigBarza <francesco.b99@hotmail.it>
2022-08-24 18:57:18 +0200

    First custom Tetra effect: Aquatic While underwater, grants increased swim speed, gives night visiona and prevents the air meter from going below one bubble.

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -18,3 +18,16 @@ onEvent('item.tags', event => {
 	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
 	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
 })
+
+onEvent('player.tick', event => {
+	user = event.getPlayer()
+    item = user.getHeldItem(MAIN_HAND)
+    nbt = item.getNbtString()
+    if(nbt.includes("double/head_left:water_stone") || nbt.includes("double/head_left:water_stone")){
+        if(user.isInWater()){
+        if(user.getAirSupply()<20)user.setAirSupply(20)
+        eff = user.getPotionEffects()
+        if(eff.getDuration("night_vision") < 220) eff.add("night_vision", 300, 0, false, false)
+        }
+    }
+})
