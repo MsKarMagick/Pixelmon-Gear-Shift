b4187fb18d2ece469c43302c66a95f7d5c8d882c
MsKarMagick <karmagickacc1@gmail.com>
2022-08-31 20:25:24 -0400

    Ball discs, configs, sheets, fossils, unfinished unfinished balls

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -4,9 +4,11 @@ console.info('Hello, World! (You will only see this line once in console, during
 
 // ITEM REGISTRY EVENT
 onEvent('item.registry', event => {
+	
+	//Origin Ore
 	event.create('origin_ore')
 	     .displayName('§6Origin Ore§r')
-
+    //Chicken Items
 	event.create('chicken_nugget_raw')
 	     .displayName('Raw Chicken Nugget')
 		 .food(f => {
@@ -39,6 +41,7 @@ onEvent('item.registry', event => {
 			f.effect('minecraft:regeneration', 5, 3, 100)
 		 })
 
+		 //Lava Cookie Variants
 		 event.create('lava_cookie_sugar')
 	     .displayName('Powdered Lava Cookie')
 		 .food(f => {
@@ -128,15 +131,48 @@ onEvent('item.registry', event => {
 		 })
 	
 	//Pixelmon Ball related items
-	//Bases
-	event.create('unpressed_iron_base').displayName('Unpressed Iron Base')
-    event.create('unpressed_silver_base').displayName('Unpressed Silver Base')
-	event.create('unpressed_aluminium_base').displayName('Unpressed Aluminium Base')
-	event.create('unpressed_platinum_base').displayName('Unpressed Platinum Base')
+	//Base Discs
+	event.create('disc_base_iron').displayName('Iron Disc')
+    event.create('disc_base_silver').displayName('Silver Disc')
+	event.create('disc_base_aluminium').displayName('Aluminium Disc')
+	event.create('disc_base_platinum').displayName('Platinum Disc')
 	event.create('unsanded_wooden_base').displayName('Unsanded Wooden Base')
-    //Plates
-	event.create('silver_plate').displayName('Silver Plate')
-	event.create('platinum_plate').displayName('Platinum Plate')
+
+	//Lid Discs
+	event.create('disc_ball_beast').displayName('Beast Ball Disc')
+    event.create('disc_ball_dive').displayName('Dive Ball Disc')
+	event.create('disc_ball_dusk').displayName('Dusk Ball Disc')
+	event.create('disc_ball_fast').displayName('Fast Ball Disc')
+	event.create('disc_ball_friend').displayName('Friend Ball Disc')
+	event.create('disc_ball_great').displayName('Great Ball Disc')
+	event.create('disc_ball_heal').displayName('Heal Ball Disc')
+	event.create('disc_ball_heavy').displayName('Heavy Ball Disc')
+	event.create('disc_ball_level').displayName('Level Ball Disc')
+	event.create('disc_ball_love').displayName('Love Ball Disc')
+	event.create('disc_ball_lure').displayName('Lure Ball Disc')
+	event.create('disc_ball_luxury').displayName('Luxury Ball Disc')
+	event.create('disc_ball_moon').displayName('Moon Ball Disc')
+	event.create('disc_ball_nest').displayName('Nest Ball Disc')
+	event.create('disc_ball_net').displayName('Net Ball Disc')
+	event.create('disc_ball_poke').displayName('Poké Ball Disc')
+	event.create('disc_ball_premier').displayName('Premier Ball Disc')
+	event.create('disc_ball_quick').displayName('Quick Ball Disc')
+	event.create('disc_ball_repeat').displayName('Repeat Ball Disc')
+	event.create('disc_ball_safari').displayName('Safari Ball Disc')
+	event.create('disc_ball_sport').displayName('Sport Ball Disc')
+	event.create('disc_ball_ultra').displayName('Ultra Ball Disc')
+
+    //Sheets
+	event.create('sheet_silver').displayName('Silver Sheet')
+	event.create('sheet_platinum').displayName('Platinum Sheet')
+	event.create('sheet_aluminium').displayName('Aluminium Sheet')
+
+	//Unfinished Tests
+	event.create('unfinished_ball_ultra').type('create:sequenced_assembly').displayName('Unassembled Ultra Ball')
+	event.create('unfinished_lid_ultra').type('create:sequenced_assembly').displayName('Unshaped Ultra Ball Lid')
+	  
+
+
 
 })
 
